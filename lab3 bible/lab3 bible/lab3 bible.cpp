7eb010bdead78cb95209748930d9eb6457ca2e98
Гестя <54850245+Gestya@users.noreply.github.com>
2019-12-16 18:20:18 +0200

    Add files via upload

---
+++ lab3 bible/lab3 bible/lab3 bible.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(640, 640), "SFMLworks");
+
+	sf::Image m_image;
+	m_image.loadFromFile("image.jpg");
+
+	sf::Texture m_ge;
+	m_ge.loadFromImage(m_image);
+
+	sf::Sprite shape;
+	shape.setTexture(m_ge);
+
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
